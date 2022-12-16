d78281a8388200e518872961e4dd4bed781ece0e
Vladimir Yazydzhi <yazydzhi@gmail.com>
2022-12-16 21:08:22 +0400

    add RNUILib Design https://wix.github.io/react-native-ui-lib/

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { TextInput } from 'react-native';
+//import { View, Text } from 'react-native';
+import { TextInput,ActivityIndicator, StyleSheet } from 'react-native';
 import axios from 'axios';
 
+//import {Component} from 'react';
+import {View, TextField, Text, Button} from 'react-native-ui-lib';
+
 const CURRENCIES_API_URL = 'https://api.apilayer.com/exchangerates_data/latest';
+
+const INPUT_SPACING = 10;
 
 const App = () => {
   const [state, setState] = React.useState({
@@ -25,7 +30,6 @@ const App = () => {
           apikey: 'YJgSxDxtZvVJAoAD8PPsUlSjPZkRBO7G'
         }
       });
-  
       setRates(response.data.rates);
     } catch (error) {
       console.error('Error fetching rates:', error);
@@ -58,21 +62,74 @@ const App = () => {
     setState((prevState) => ({ ...prevState, rubles }));
     calculateValues(rubles);
   };
-  
 
-  return (
-    <View style={{ backgroundColor: '#ebebeb', width: '100%', height: '100%', alignItems: 'center', alignItems: 'center', justifyContent: 'center' }}>
-      <TextInput keyboardType='numeric' placeholder='Введи рубли'
-        value={state.rubles} //.toLocaleString('ru-RU', {style: 'currency', currency: 'RUB',minimumFractionDigits:0})}
-        onChangeText={onChangeText}
-      />
+  if (!rates){
+    return (
+      <View flex paddingH-25 paddingT-120 >
+        <Text blue50 text20 containerStyle={{marginBottom: INPUT_SPACING}}>Конвертик</Text>
+        <Text text60 containerStyle={{marginBottom: INPUT_SPACING}}>Загрузка курса валют</Text>
+        <ActivityIndicator size="large"/>
+        
+      </View>
+    );
+  }
+  else{
+    return (
+      <View flex paddingH-25 paddingT-120>
+          <Text blue50 text20>Конвертик</Text>
+          {/* <TextField 
+            text50
+            placeholder='Введи рубли'
+            containerStyle={{marginBottom: INPUT_SPACING}}
+            keyboardType='numeric' 
+            //floatingPlaceholder 
+            helperText="this is an helper text"
+            value={state.rubles}//.toLocaleString('ru-RU', {style: 'currency', currency: 'RUB',minimumFractionDigits:0})}
+            onChangeText={onChangeText}
 
-      <Text>Рубли: {state.rubles.toLocaleString('ru-RU', {style: 'currency', currency: 'RUB'})}</Text>
+          /> */}
+          <TextField
+            text50
+            containerStyle={{marginBottom: INPUT_SPACING}}
+            floatingPlaceholder
+            placeholder="Рубли"
+          //  rightIconSource='MM'
+            onChangeText={onChangeText}
+            value={state.rubles.toString()}//.toLocaleString('ru-RU', {style: 'currency', currency: 'RUB'})}
+          />
+           {/* <Text>{rates['GEL']}</Text> */}
+          <TextField
+            text60
+            containerStyle={{marginBottom: INPUT_SPACING}}
+            floatingPlaceholder
+            placeholder={'Лари ('+rates['GEL'].toFixed(2)+')'}
+            //multiline
+            value={state.lari.toFixed(2).toString()}//.toLocaleString('ka-GE', {style: 'currency', currency: 'GEL'})}
+          />
+          <TextField
+            text60
+            containerStyle={{marginBottom: INPUT_SPACING}}
+            floatingPlaceholder
+            placeholder={'Евро ('+rates['EUR'].toFixed(2)+')'}
+            //multiline
+            value={state.euro.toFixed(2).toString()}//.toLocaleString('de-DE', {style: 'currency', currency: 'EUR'})}
+          />
+          <TextField
+            text60
+            containerStyle={{marginBottom: INPUT_SPACING}}
+            floatingPlaceholder
+            placeholder={'Доллары ('+rates['USD'].toFixed(2)+')'}
+            //multiline
+            value={state.dollars.toFixed(2).toString()}//.toLocaleString('en-US', {/*format: '#,##0.00 ¤', */style: 'currency', currency: 'USD'})}
+          />
+      {/* <Text>Рубли: {state.rubles.toLocaleString('ru-RU', {style: 'currency', currency: 'RUB'})}</Text>
       <Text>Лари: {state.lari.toLocaleString('ka-GE', {style: 'currency', currency: 'GEL'})}</Text>
       <Text>Евро: {state.euro.toLocaleString('de-DE', {style: 'currency', currency: 'EUR'})}</Text>
-      <Text>Доллары: {state.dollars.toLocaleString('en-US', {/*format: '#,##0.00 ¤', */style: 'currency', currency: 'USD'})}</Text>
+      <Text>Доллары: {state.dollars.toLocaleString('en-US', {/*format: '#,##0.00 ¤', style: 'currency', currency: 'USD'})}</Text> */}
     </View>
+    
   );
+        };
 };
 
 export default App;
