408c268ecbed68e7d8eb1af71467cea2e3aa76cf
Vladimir Yazydzhi <yazydzhi@gmail.com>
2023-01-08 17:26:41 +0400

    refactor code. Add delition, change on list item

--- home.component.js
+++ home.component.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import { Text, Button, Icon, Divider, Layout, TopNavigation, TopNavigationAction, Input  } from '@ui-kitten/components';
+import React, { useEffect } from 'react';
+import { SafeAreaView, StyleSheet, ScrollView } from 'react-native';
+import { Text, Button, Icon, Divider, Layout, TopNavigation, TopNavigationAction, Input, ListItem, List } from '@ui-kitten/components';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { FormattedCurrency, FormattedNumber,FormattedMessage,IntlProvider} from 'react-intl';
+import { FormattedCurrency, FormattedNumber, FormattedMessage, IntlProvider } from 'react-intl';
 import translations from './translations.json';
+import { View } from 'react-native-animatable';
 
-  const InfoIcon = (props) => (
-    <Icon {...props} name='info-outline'/>
-  );
+const InfoIcon = (props) => (
+  <Icon {...props} name='info-outline' />
+);
 
-  const styles = StyleSheet.create({
-    text:{
-        marginTop: 15,
-        marginLeft: 20, 
-        marginRight: 20, 
-    },
-    input: {
-        marginTop: 10,
-        marginLeft: 20, 
-        marginRight: 20, 
-    },
-  });
+const styles = StyleSheet.create({
+  text: {
+    marginTop: 15,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  input: {
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+});
 
-  const CURRENCIES_API_URL = 'https://api.apilayer.com/exchangerates_data/latest';
+const CURRENCIES_API_URL = 'https://api.apilayer.com/exchangerates_data/latest';
 
 
 export const HomeScreen = ({ navigation }) => {
@@ -33,18 +34,61 @@ export const HomeScreen = ({ navigation }) => {
   };
 
   const navigateInfo = () => (
-    <TopNavigationAction icon={InfoIcon} onPress={navigateDetails}/>
+    <TopNavigationAction icon={InfoIcon} onPress={navigateDetails} />
   );
 
   const [state, setState] = React.useState({
-    val: {
-      ['RUB']: 1,
+    cur: {
+      ['RUB']: 0,
       ['GEL']: 0,
       ['EUR']: 0,
       ['USD']: 0,
-      ['ILS']: 0 ,
     }
   });
+
+  React.useEffect(() => {
+    async function loadCurrencies() {
+      // получение данных из AsyncStorage
+      const currenciesString = await AsyncStorage.getItem('currenciesList');
+      let currenciesList = JSON.parse(currenciesString);
+
+      // если данные существуют, обновляем состояние
+      if (currenciesList) {
+        setState({ cur: currenciesList });
+      } else {
+        // данных нет, сохраняем исходный список валют в AsyncStorage
+        await AsyncStorage.setItem('currenciesList', JSON.stringify(state.cur));
+      }
+    }
+
+    loadCurrencies();
+  }, []);
+
+  // функция добавления новой валюты
+  const addCurrency = async (currency) => {
+    // добавление новой валюты
+    const newCurrency = { [currency]: 0 };
+    setState((prevState) => ({
+      cur: { ...prevState.cur, ...newCurrency }
+    }));
+
+    // сохранение обновленного списка валют в AsyncStorage
+    await AsyncStorage.setItem('currenciesList', JSON.stringify(state.cur));
+  };
+  const removeCurrency = (currency) => {
+    // удаление валюты
+    const { [currency]: _, ...newCurrencies } = state.cur;
+    setState({ cur: newCurrencies });
+  
+    console.log("DELETE currency:", currency, "NEW cur:", (state.cur), "NEW { cur: newCurrencies }", {cur: newCurrencies });
+  };
+  
+  React.useEffect(() => {
+    // сохранение обновленного списка валют в AsyncStorage
+
+   // AsyncStorage.removeItem('currenciesList');
+    AsyncStorage.setItem('currenciesList', JSON.stringify(state.cur));
+  }, [state.cur]);
 
   const [rates, setRates] = React.useState(null);
 
@@ -58,44 +102,45 @@ export const HomeScreen = ({ navigation }) => {
           apikey: '6rOLEhF1PKO8tbicWoLG9wCXSRwlE3hk'
         }
       });
-      setRates(response.data.rates);
 
       response.data.rates.update_date = new Date().toISOString(); // Добавляем текущую дату
-      await AsyncStorage.setItem('key', JSON.stringify(response.data.rates)); // записываем в AsSt
-      console.log("UPDATED:",JSON.stringify(response.data.rates.update_date));
+      await AsyncStorage.setItem('exchange', JSON.stringify(response.data.rates)); // записываем в AsSt
+     // console.log("UPDATED:", JSON.stringify(response.data.rates.update_date));
+
+      setRates(response.data.rates);
 
     } catch (error) {
       console.error('Error fetching rates:', error);
     }
   };
 
-  const updateCurrencies= async () => {
+  const updateCurrencies = async () => {
     try {
-      const value = await AsyncStorage.getItem('key'); // получение данных
+      const exchengeRate = await AsyncStorage.getItem('exchange'); // получение данных
 
-      if (value !== null) {
-        const object = JSON.parse(value);
+      if (exchengeRate !== null) {
+        const exchange = JSON.parse(exchengeRate);
 
         const toDay = new Date();
-        const updDay = new Date(object.update_date.toString());
+        const updDay = new Date(exchange.update_date.toString());
         toDay.setDate(toDay.getDate() - 1);
-        if (toDay.getTime() < updDay.getTime()){ // разница между сохраненным значением и сегодняшней датой меньше суток
-         // console.log("toDay < updDay ! Use AsSt");
-          setRates(object);
+        if (toDay.getTime() < updDay.getTime()) { // разница между сохраненным значением и сегодняшней датой меньше суток
+          // console.log("toDay < updDay ! Use AsSt");
+          setRates(exchange);
         } else { // разница между сохраненным значением и сегодняшней датой больше суток
           //console.log("toDay >= updDay ! Use fetchCurrencies");
           fetchCurrencies();
         }
       } else {
         fetchCurrencies();
-      } 
+      }
 
     } catch (error) {
       console.error('Error updating rates:', error);
     }
   };
 
-  React.useEffect(() =>{
+  React.useEffect(() => {
     //fetchCurrencies();
     updateCurrencies();
   }, []);
@@ -103,27 +148,27 @@ export const HomeScreen = ({ navigation }) => {
   const checkVal = (text) => { // проверка входного значения
     const MAX_RUBLES = 9223372036854775807; // максимальное для int
 
-    let val = parseInt(text);
-    if (isNaN(val)) {
-      val = 0;
+    let inputCur = parseInt(text);
+    if (isNaN(inputCur)) {
+      inputCur = 0;
     }
-    if (val > MAX_RUBLES) {
-      val = MAX_RUBLES;
+    if (inputCur > MAX_RUBLES) {
+      inputCur = MAX_RUBLES;
     }
-    return val;
+    return inputCur;
   }
 
   const onChange = (text, curCode) => {
     text = checkVal(text);
-  
+
     setState(prevState => {
       const newState = { ...prevState };
-      for (const currency in rates) {
-        newState.val[currency] = rates[currency] * (text / rates[curCode]);
-        if (currency===curCode){
-          newState.val[currency] = newState.val[currency].toFixed(0);
+      for (const currency in prevState.cur) { // rates OR cur
+        newState.cur[currency] = rates[currency] * (text / rates[curCode]);
+        if (currency === curCode) {
+          newState.cur[currency] = newState.cur[currency].toFixed(0);
         } else {
-          newState.val[currency] = newState.val[currency].toFixed(4);
+          newState.cur[currency] = newState.cur[currency].toFixed(4);
         }
 
       }
@@ -139,7 +184,7 @@ export const HomeScreen = ({ navigation }) => {
   //       currency={currencyCode}
   //     />
   //   );
-  
+
   //   return currencyString;
   // };
 
@@ -149,7 +194,7 @@ export const HomeScreen = ({ navigation }) => {
   //     currency={currencyCode}
   //   />
   // );
-  
+
 
   const CurrencyName = ({ currencyCode }) => {
     const currencyName = (
@@ -158,78 +203,62 @@ export const HomeScreen = ({ navigation }) => {
         defaultMessage={currencyCode}
       />
     );
-  
+
     return currencyName;
   };
-  
-  if (!rates){
+
+  const DelIcon = (props) => (
+    <Icon {...props} name='trash-outline'/>
+  );
+
+  if (!rates) {
     return (
       <SafeAreaView style={{ flex: 1 }}>
-        <TopNavigation title='Конвертик' alignment='center' accessoryRight={navigateInfo}/>
-        <Divider/>
+        <TopNavigation title='Конвертик' alignment='center' accessoryRight={navigateInfo} />
+        <Divider />
         <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Text style={styles.text} category='h1'>Загрузка</Text>
         </Layout>
       </SafeAreaView>
     );
   }
-  else{
+  else {
     return (
       <IntlProvider messages={translations} locale='en'>
-    <SafeAreaView style={{ flex: 1 }}>
-      <TopNavigation title='Конвертик' alignment='center' accessoryRight={navigateInfo}/>
-      <Divider/>
-      <Layout style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start' }}>
-        <Text style={styles.text} category='s1'>Рубли {state.val['RUB'].toString()}</Text>
-        <Input
-            style={styles.input}
-            placeholder=''
-            size='medium'
-            keyboardType='numeric' 
-            clearTextOnFocus
-            onChangeText={(text) => onChange(text, 'RUB')}
-         // keyboardType="decimal-pad"
-            value={state.val['RUB'].toString()}
-        />
-        <Text style={styles.text} category='s1'><CurrencyName currencyCode="GEL" />{state.val['GEL'].toString()}</Text>
-        <Input
-            style={styles.input}
-            size='medium'
-            keyboardType='numeric' 
-            clearTextOnFocus
-            onChangeText={(text) => onChange(text, 'GEL')}
-            value={state.val['GEL'].toString()}
-        />
-        <Text style={styles.text} category='s1'>Евро {state.val['EUR'].toString()}</Text>
-        <Input
-            style={styles.input}
-            size='medium'
-            keyboardType='numeric' 
-            clearTextOnFocus
-            onChangeText={(text) => onChange(text, 'EUR')}
-            value={state.val['EUR'].toString()}
-        />
-        <Text style={styles.text} category='s1'>Доллары {state.val['USD'].toString()}</Text>
-        <Input
-            style={styles.input}
-            size='medium'
-            keyboardType='numeric' 
-            clearTextOnFocus
-            onChangeText={(text) => onChange(text, 'USD')}
-            value={state.val['USD'].toString()}
-        />
-        <Text style={styles.text} category='s1'>Шекели {state.val['ILS'].toString()}</Text>
-        <Input
-            style={styles.input}
-            size='medium'
-            keyboardType='numeric' 
-            clearTextOnFocus
-            onChangeText={(text) => onChange(text, 'ILS')}
-            value={state.val['ILS'].toString()}
-        />
-      </Layout>
-    </SafeAreaView>
-    </IntlProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <TopNavigation title='Конвертик' alignment='center' accessoryRight={navigateInfo} />
+          <Divider />
+          <ScrollView>
+            <Layout style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+              {Object.entries(state.cur).map(([currency, value]) => (
+                <ListItem
+                  key={currency}
+                  title={<CurrencyName currencyCode={currency} />}
+                  accessoryRight={() => <Icon name='trash-2-outline' onPress={() => removeCurrency(currency)} />}
+                >
+                  <Text style={styles.text} category='s1'>
+                    <CurrencyName currencyCode={currency} />
+                  </Text>
+                  <Input
+                    style={styles.input}
+                    size='medium'
+                    keyboardType='numeric'
+                    clearTextOnFocus
+                    onChangeText={(text) => onChange(text, currency)}
+                    value={value.toString()}
+                  />
+                  <Button 
+                    name='trash-2-outline2' 
+                    onPress={() => removeCurrency(currency)}
+                    accessoryLeft={DelIcon}
+                    appearance='ghost'
+                  />
+                </ListItem>
+              ))}
+            </Layout>
+          </ScrollView>
+        </SafeAreaView>
+      </IntlProvider>
     );
   };
 };
